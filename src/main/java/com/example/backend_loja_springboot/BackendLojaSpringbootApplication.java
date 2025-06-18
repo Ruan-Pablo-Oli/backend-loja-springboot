608514f0c00be50420a757058c011d50f04d319f
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-06-17 21:16:03 -0300

    Project created

---
+++ src/main/java/com/example/backend_loja_springboot/BackendLojaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_loja_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLojaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLojaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_loja_springboot/BackendLojaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_loja_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLojaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
