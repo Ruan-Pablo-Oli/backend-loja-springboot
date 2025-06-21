359a57994db589900191ccd3d16cb25a9977f74f
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-06-21 18:42:13 -0300

    feat:User entity created and user table created in postgreSQL

---
+++ src/main/java/com/example/backend_loja_springboot/model/User.java
@@ -0,0 +1,60 @@
+package com.example.backend_loja_springboot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true,nullable = false)
+	private String name;
+	
+	private String password;
+	
+	public User() {
+		
+	}
+	
+	
+
+	public User( String name, String password) {
+		super();
+		this.name = name;
+		this.password = password;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	
+	
+	
+}
